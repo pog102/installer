5b7ad7bc4edf2d634cb66b2947e53a916100bbb7
pog102 <85764555+pog102@users.noreply.github.com>
2022-07-31 00:01:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo pacman -Syu --noconfirm ttf-hack ttf-roboto ttf-sazanami ttf-jetbrains-mono \
 	rofi dunst python-pywal firefox git lsd rsync ncurses fftw cmake \
-    unclutter redshift udiskie udsiks2 transmission-cli mpd mpc
-
+    unclutter redshift udiskie udsiks2 transmission-cli mpd mpc neovim redshift zsh
+chsh -s /usr/bin/zsh
 sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key FBA220DFC880C036
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
@@ -15,12 +15,12 @@ cd paru
 makepkg -si --noconfirm
 cd ..
 paru -S --noconfirm cli-visualizer proton-ge-custom-bin brillo python-pywalfox
-usermod -aG vidoe $USER
+usermod -aG video $USER
 ##autologin
-mkdir /etc/systemd/system/getty@tty1.service.d/
-echo "[Service]" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
-echo "ExecStart=" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
-echo "ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin username - $TERM" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
+sudo mkdir /etc/systemd/system/getty@tty1.service.d/
+echo "[Service]" | sudo tee -a /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo "ExecStart="  | sudo tee -a /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo "ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin username - $TERM" | sudo tee -a /etc/systemd/system/getty@tty1.service.d/autologin.conf
 ##dots
 cp -r .config/ $HOME/
 cp .zshrc $HOME/
@@ -34,9 +34,11 @@ cp -r Music $HOME/
 ln -sf $HOME/.Xresources  $HOME/.cache/wal/colors.Xresources
 sudo pacman -S --noconfirm steam
 ##make dwm
-cd ~/suckless/dwm/
-sudo make install
-cd ~/suckless/st/
-sudo make install
-cd ~/suckless/dwmblocks
-sudo make install
+#cd ~/suckless/dwm/
+#sudo make install
+#cd ~/suckless/st/
+#sudo make install
+#cd ~/suckless/dwmblocks
+#sudo make install
+
+
