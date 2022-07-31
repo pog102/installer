276f1f92e2a4326aeeae4b7eac8e22bca9c03937
pog102 <85764555+pog102@users.noreply.github.com>
2022-07-31 17:46:04 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,4 +55,4 @@ sudo pacman -S steam
 #cd ~/suckless/dwmblocks
 #sudo make install
 #Footer
-sudo sed -i 's/-linux/-linux-xanmod-edge/g' /usr/share/bootctl/arch.conf
+sudo sed -i 's/-linux/-linux-xanmod-edge/g' /boot/loader/entries/*
