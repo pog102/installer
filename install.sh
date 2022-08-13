93b6b6e8a97917db683ddee4bdc430f3cf63fcbf
pog102 <85764555+pog102@users.noreply.github.com>
2022-08-13 16:04:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ brillo -c -S 1
 ##autologin
 sudo mkdir /etc/systemd/system/getty@tty1.service.d/
 sudo touch /etc/systemd/system/getty@tty1.service.d/autologin.conf
-echo "ATTRS{idVendor}==\"152d\", ATTRS{idProduct}==\"2329\", RUN+=\"/tmp/test.sh\"" | sudo tee -a /etc/udev/rules.d/test.rules
+echo "ATTRS{idVendor}==\"152d\", ATTRS{idProduct}==\"2329\", RUN+=\"/tmp/nyaupdate.sh\"" | sudo tee -a /etc/udev/rules.d/test.rules
 
 echo "[Service]" | sudo tee -a /etc/systemd/system/getty@tty1.service.d/autologin.conf
 echo "ExecStart="  | sudo tee -a /etc/systemd/system/getty@tty1.service.d/autologin.conf
