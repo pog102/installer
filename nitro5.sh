79a3e3dae5f4defb4e9a5ffb44a8b8942f5f72d0
pog102 <85764555+pog102@users.noreply.github.com>
2022-10-07 23:06:36 +0300

    Update nitro5.sh

--- nitro5.sh
+++ nitro5.sh
@@ -27,7 +27,7 @@ echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 echo "[multilib]" | sudo tee -a /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
 
-paru -S  cli-visualizer nsxiv zaread autotiling brillo python-pywalfox linux-xanmod-edge linux-xanmod-edge-headers proton-ge-custom \
+paru -S  cli-visualizer python-pywalfox nsxiv zaread autotiling brillo python-pywalfox linux-xanmod-edge linux-xanmod-edge-headers proton-ge-custom \
 autojump pamixer transmission-remote-tui-git sfeed clifm
 sudo usermod -aG video $USER
 brillo -c -S 1
