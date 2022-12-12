f986e11d1cd2f74f3630300fdfe2b678999a6f0e
pog102 <85764555+pog102@users.noreply.github.com>
2022-12-12 19:51:06 +0200

    Update nitro5.sh

--- nitro5.sh
+++ nitro5.sh
@@ -21,7 +21,8 @@ bluez bluez-utils unclutter redshift udiskie udisks2 transmission-cli mpd mpc ne
 mpv feh fzf yt-dlp unclutter nvidia-open-dkms neovim zip unzip zathura-pdf-poppler \
 xorg-server xorg-xinit \
 lib32-libglvnd lib32-nvidia-utils lib32-sdl12-compat \
-xclip maim isync polybar networkmanager rclone papirus-icon-theme zsh-autosuggestions
+xclip maim isync polybar networkmanager rclone papirus-icon-theme \
+zsh-autosuggestions zsh-syntax-highlighting
 sudo systemctl enable NetworkManager
 chsh -s /usr/bin/zsh
 systemctl enable bluetooth.service
