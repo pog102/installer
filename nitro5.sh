2a9c691b579ba67640f65413045697e138ab32c1
pog102 <85764555+pog102@users.noreply.github.com>
2022-12-23 12:30:30 +0200

    Update nitro5.sh

--- nitro5.sh
+++ nitro5.sh
@@ -126,7 +126,7 @@ ln -fs $HOME/.cache/wal/colors.css $HOME/.config/firefox/chrome/styles/colors.cs
 ln -fs $HOME/.cache/wal/gtkrc $HOME/.themes/wal/gtk-2.0/gtkrc
 
 
-#nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 sed -i 's/background.*//g' ~/.local/share/nvim/site/pack/packer/start/pywal.nvim/lua/pywal/core.lua
 sudo pywalfox install
 sudo xset b off
