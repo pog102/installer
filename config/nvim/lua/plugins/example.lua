a0c1cba56ee27ef514902d077ad48904f981a892
Your Name <you@example.com>
2023-03-04 22:04:37 +0200

    ss

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
--- if true then return {} end
+if true then return {} end
 
 -- every spec file under config.plugins will be loaded automatically by lazy.nvim
 --
@@ -10,15 +10,15 @@
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "pywal",
+      colorscheme = "gruvbox",
     },
   },
-  { "AlphaTechnolog/pywal.nvim" },
 
   -- change trouble config
   {
@@ -93,10 +93,6 @@ return {
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
-        csharp_ls = {},
-        bashls = {},
-        marksman = {},
-
       },
     },
   },
@@ -146,20 +142,20 @@ return {
     opts = {
       ensure_installed = {
         "bash",
-        -- "help",
+        "help",
         "html",
-        -- "javascript",
-        -- "json",
+        "javascript",
+        "json",
         "lua",
         "markdown",
         "markdown_inline",
         "python",
-        -- "query",
+        "query",
         "regex",
-        -- "tsx",
-        -- "typescript",
-        -- "vim",
-        -- "yaml",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
       },
     },
   },
@@ -172,8 +168,8 @@ return {
     opts = function(_, opts)
       -- add tsx and treesitter
       vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
+          "tsx",
+          "typescript",
       })
     end,
   },
@@ -212,7 +208,6 @@ return {
         "stylua",
         "shellcheck",
         "shfmt",
-        "npm",
         "flake8",
       },
     },
